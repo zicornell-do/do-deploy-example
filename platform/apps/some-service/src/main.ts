11c6817690acdba4218494cd9a30beee41c1629e
Topher ZiCornell <tzicornell@deeporigin.com>
2025-09-25 10:46:40 -0500

    feat: new sample service
    * Added a simple "hello-world" service to use as an example for building and deploying.

---
+++ platform/apps/some-service/src/main.ts
@@ -0,0 +1,30 @@
+import 'reflect-metadata';
+import { NestFactory } from '@nestjs/core';
+import { Module, Controller, Get } from '@nestjs/common';
+
+@Controller()
+class AppController {
+  @Get('/')
+  getRoot(): string {
+    return 'Hello, World!';
+  }
+}
+
+@Module({
+  controllers: [AppController],
+})
+class AppModule {}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+  await app.listen(port);
+  // eslint-disable-next-line no-console
+  console.log(`some-service listening on http://localhost:${port}`);
+}
+
+bootstrap().catch((err) => {
+  // eslint-disable-next-line no-console
+  console.error('Failed to start application', err);
+  process.exit(1);
+});
