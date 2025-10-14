d1df0c191d180e85f1423f23030a5d6a53989408
Topher ZiCornell <tzicornell@deeporigin.com>
2025-10-14 10:53:57 -0500

    fix: cleanup unused terraform
    * Removed the app-level terraform stuff since all that is handled by helm.

--- platform/apps/some-service/deploy/terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "argo_app_name" {
-  value       = var.app_name
-  description = "Argo CD application name"
-}
-
-output "namespace" {
-  value       = var.namespace
-  description = "Namespace for the workload"
-}
